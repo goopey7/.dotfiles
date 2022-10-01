afdd496ed56c04698f51905744e8c5dbc3aa3ec2
Sam Collier <sam@samcollier.tech>
2022-10-01 03:41:46 +0100

    make alt x do nothing

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -767,14 +767,9 @@ globalkeys = my_table.join(
               {description = "show the menubar", group = "super"})
     --]]
 
+	
     awful.key({ altkey }, "x",
               function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
               end,
               {description = "lua execute prompt", group = "awesome"})
     --]]
