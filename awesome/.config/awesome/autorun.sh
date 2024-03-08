324bd7a20c0ee3cb65d90af35fa6253419c5ed05
Sam Collier <samcollier916@gmail.com>
2024-03-08 16:23:50 +0000

    laptop specific changes

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -11,3 +11,8 @@ ckb-next -b &
 run xbanish
 # map caps key to escape
 setxkbmap -option caps:escape
+run nm-applet
+picom
+xinput set-prop "$(xinput list --name-only | grep -i touch)" "libinput Tapping Enabled" 1
+xinput set-prop 9 340 0
+xinput set-prop 11 340 0
