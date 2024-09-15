f280f214ff4c9f645dba53052555aaf1359c1ec1
Sam Collier <samcollier916@gmail.com>
2024-09-15 16:02:45 +0100

    launch powershell 7

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -52,7 +52,7 @@ local config = {
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "powershell.exe" }
+	config.default_prog = { "pwsh.exe" }
 end
 
 return config
